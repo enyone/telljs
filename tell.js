780e1f4c6d709072386d57c758221d224a7bd362
enyone <enyone@rainio.org>
2013-11-19 20:48:02 +0200

    add

--- tell.js
+++ tell.js
@@ -1,2 +1,14 @@
 var express = require('express');
+var asyncblock = require('asyncblock');
+var exec = require('child_process').exec;
+
 var app = express();
+
+app.get('/devices', function(req, res) {
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+});
+
+app.listen(process.env.PORT || 8080);
